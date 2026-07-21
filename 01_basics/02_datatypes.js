b491470b88dfb6230afdb4bbb6f147958d2fc2c5
niikkk07 <nikhilsinghsolanki238@gmail.com>
2026-07-21 11:53:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"
+
+//aleart(3+3)   //not worked because we are using nodejs, not browser
+console.log(3 
+         + 3)    //code redability should be high
+
+console.log("Nikk")
+let isloggedin = true
+
+console.log("isloggedin stored " ,isloggedin)
+
+let name = "Nikk"
+let age = 20
+ isloggedin = false
+let state;  // undefined
+
+    // number =>  2 to power 53
+    //bigint
+    // string => ""
+    // boolean => True/False
+    //null => object type (standalone value)   e.g. like temprature
+    //undefined =>  undefined type
+    //symbol =>  unique 
+
+    //object 
+  console.log("typof isloggedin is " ,typeof isloggedin)
+    console.log("typof state is " ,typeof state)
+    console.log("typof age is " ,typeof age)
